cacfb03d940e753c5d4daea0d1803c84eec8cfd9
David Cardozo <gd.cardozo684@uniandes.edu.co>
2019-05-18 17:32:18 -0500

    feat: adding v2 of class

--- scripts/exploration.py
+++ scripts/exploration.py
@@ -2,15 +2,16 @@ import os
 import numpy as np
 import tensorflow as tf
 
+from tf_classifier import TLClassifier
+
 PATH_TO_SAVED_MODEL = os.path.join("/capstone_models/mobilenetv1", "saved_model")
 assert os.path.isdir(PATH_TO_SAVED_MODEL)
 
+IMAGE_BATCH = 5
 # Image noisey
-img = np.random.rand(1, 100, 100,3) # Batch, size, channels
+img = np.random.rand(IMAGE_BATCH, 100, 100,3) # Batch, size, channels
 
-# Predictor
-predictor = tf.contrib.predictor.from_saved_model(PATH_TO_SAVED_MODEL)
-prediction_dict = predictor({"inputs": img})
-
-print(prediction_dict)
+classifier = TLClassifier(PATH_TO_SAVED_MODEL)
+example_result = classifier.get_classification(img, threshold=0.01)
+print example_result
 
--- tf_classifier/tf_model.py
+++ tf_classifier/tf_model.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 import tensorflow as tf
 
 class TLClassifier(object):
@@ -7,7 +8,7 @@ class TLClassifier(object):
         self.predictor = tf.contrib.predictor.from_saved_model(saved_model_path)
 
 
-    def get_classification(self, image):
+    def get_classification(self, image, threshold=0.6):
         """Determines the color of the traffic light in the image
 
         Args:
@@ -17,11 +18,29 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        assert len(image.shape) == 4, "image should be rank 4, with batch dimmesion on the first dimmension"
-        predictions = self.predictor({"inputs": image})
+        assert len(image.shape) == 4, "image should be rank 4, with batch dimmesion on the first dimension"
+        raw_predictions = self.predictor({"inputs": image})
+        predictions = self.parse_predictions(raw_predictions, threshold)
+
+        # TODO: Add logic to decide which traffic light are we seeing based on predictions
+
+        return predictions
 
     @staticmethod
-    def parse_predictions(predictions):
+    def parse_predictions(predictions, threshold=0.6):
         assert type(predictions) == dict, "Prediction type is {}, not dict".format(type(predictions))
-        # TODO: Finish up
-        raise NotImplementedError
+        frames = predictions['num_detections'].shape[0]
+
+        evidence = []
+        for frame_index in xrange(frames):
+            bool_index = np.greater_equal(predictions['detection_scores'][frame_index], threshold)
+
+            detection_classes = predictions['detection_classes'][frame_index][bool_index]
+            detection_scores = predictions['detection_scores'][frame_index][bool_index]
+            evidence_frame = zip(detection_classes, detection_scores)
+
+            evidence.append(evidence_frame)
+
+        return evidence
+
+
